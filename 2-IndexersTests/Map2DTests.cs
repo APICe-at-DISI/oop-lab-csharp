672e79421ff2e02321e8a3f163b3f41a79f638da
APlacuzzi <andrea.placuzzi@unibo.it>
2020-12-10 10:26:04 +0100

    replace main class with test class

---
+++ 2-IndexersTests/Map2DTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Indexer.Tests
+{
+    [TestClass]
+    public class Map2DTests
+    {
+        private IMap2D<int, int, int> pitagoricTable;
+        private readonly int[] seven = new int[] { 7, 14, 21, 28, 35, 42, 49, 56, 63, 70 };
+
+        // Called once before each test after the constructor
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            pitagoricTable = new Map2D<int, int, int>();
+            pitagoricTable.Fill(
+                Enumerable.Range(1, 10),
+                Enumerable.Range(1, 10),
+                (i, j) => i * j
+            );
+        }
+
+        [TestMethod]
+        public void FillAndIndexerTest()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Assert.AreEqual(i * i, pitagoricTable[i, i]);
+            }
+        }
+
+        [TestMethod]
+        public void GetRowTest()
+        {
+            if (!pitagoricTable.GetRow(7).Select(t => t.Item2).SequenceEqual(seven))
+            {
+                Assert.Fail("Wrong implementation");
+            }
+        }
+
+        [TestMethod]
+        public void GetColumnTest()
+        {
+            if (!pitagoricTable.GetColumn(7).Select(t => t.Item2).SequenceEqual(seven))
+            {
+                Assert.Fail("Wrong implementation");
+            }
+        }
+    }
+}
